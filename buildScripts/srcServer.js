943ff2eca479e068c1cf8cb4711819971554a210
Sameh Abouelsaad <sameh@Samehs-MacBook-Pro.local>
2020-06-26 00:53:32 +0200

    clean

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,7 +3,6 @@ import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
-
 var port = 3000;
 var app = express();
 var compiler = webpack(config);
@@ -16,6 +15,14 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
+app.get('/users', function(req, res) {
+  res.json([
+    {id: 1, firstName:"sameh", lastName:"farouk", email:"samehabouelsaad@gmail.com"},
+    {id: 2, firstName:"ahmed", lastName:"hany", email:"some@some.org"},
+    {id: 3, firstName:"mohamed", lastName:"galal", email:"f.seed@moore.com"}
+  ])
+});
+
 
 app.listen(port, function(err) {
   if (err) {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import numeral from 'numeral';
+
 import './bootstrap.css';
 
 const courseValue= numeral(1000).format('$0,0.00')

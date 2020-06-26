1ae2fea6c9c68a7826552ab7174fc7ded081ce46
Sameh Abouelsaad <sameh@Samehs-MacBook-Pro.local>
2020-06-26 17:35:17 +0200

    end of module 10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import numeral from 'numeral';
+import {getUsers, delUser} from './api/userApi';
 
 import './bootstrap.css';
 
-const courseValue= numeral(1000).format('$0,0.00')
-console.log(`I would pay ${courseValue} for this awsome course!`) //eslint-disable-line no-console
+getUsers().then(res => {
+  const tbody = document.getElementById('users')
+  res.forEach(user => {
+    const row = document.createElement('tr');
+    const delLink = document.createElement('td');
+    delLink.innerHTML = `<a href="#" class="deleteUser" data-id="${user.id}">Delete</a>`
+    row.appendChild(delLink);
+    for (let nfo of Object.entries(user)) {
+      const cell = document.createElement('td');
+      cell.innerText = nfo[1];
+      row.appendChild(cell)
+    }
+    tbody.appendChild(row);
+  })
+  const deleteLinks = document.getElementsByClassName('deleteUser');
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      event.preventDefault();
+      const element = event.target;
+      delUser(element.attributes["data-id"].value);
+      const row = element.parentElement.parentElement;
+      row.parentNode.removeChild(row);
+    }
+  })
+})
